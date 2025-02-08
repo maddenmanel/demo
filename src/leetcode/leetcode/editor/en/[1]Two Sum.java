b818df4a4a8383cc67eaa6a3ed9347d35f933a35
Madden <130027037+maddenmanel@users.noreply.github.com>
2025-02-08 14:46:29 +0800

    Delete out .class file.

--- src/leetcode/leetcode/editor/en/[1]Two Sum.java
+++ src/leetcode/leetcode/editor/en/[1]Two Sum.java
@@ -1,56 +1,68 @@
-//Given an array of integers nums and an integer target, return indices of the 
-//two numbers such that they add up to target. 
+package leetcode.leetcode.editor.en;//Given an array of integers nums and an integer target, return indices of the
+//two numbers such that they add up to target.
 //
-// You may assume that each input would have exactly one solution, and you may 
-//not use the same element twice. 
+// You may assume that each input would have exactly one solution, and you may
+//not use the same element twice.
 //
-// You can return the answer in any order. 
+// You can return the answer in any order.
 //
-// 
-// Example 1: 
 //
-// 
+// Example 1:
+//
+//
 //Input: nums = [2,7,11,15], target = 9
 //Output: [0,1]
 //Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-// 
 //
-// Example 2: 
 //
-// 
+// Example 2:
+//
+//
 //Input: nums = [3,2,4], target = 6
 //Output: [1,2]
-// 
 //
-// Example 3: 
 //
-// 
+// Example 3:
+//
+//
 //Input: nums = [3,3], target = 6
 //Output: [0,1]
-// 
 //
-// 
-// Constraints: 
 //
-// 
-// 2 <= nums.length <= 10⁴ 
-// -10⁹ <= nums[i] <= 10⁹ 
-// -10⁹ <= target <= 10⁹ 
-// Only one valid answer exists. 
-// 
 //
-// 
-//Follow-up: Can you come up with an algorithm that is less than 
+// Constraints:
+//
+//
+// 2 <= nums.length <= 10⁴
+// -10⁹ <= nums[i] <= 10⁹
+// -10⁹ <= target <= 10⁹
+// Only one valid answer exists.
+//
+//
+//
+//Follow-up: Can you come up with an algorithm that is less than
 //O(n²)
 // time complexity?
 //
 // Related Topics Array Hash Table 👍 59943 👎 2149
 
 
+import java.util.HashMap;
+
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class TwoSumSolution {
     public int[] twoSum(int[] nums, int target) {
-        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new TwoSumSolution().twoSum(new int[]{2, 7, 11, 15}, 9));
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
